5ade5db230c8d12a88e137507abab9ef18aa7044
phanstudio <donenoelle@gmail.com>
2025-07-12 11:08:54 +0100

    update

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -1,27 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import inject from "@rollup/plugin-inject";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    inject({
+      Buffer: ["buffer", "Buffer"]
+    })
+  ],
   define: {
-    'process.env': {},
+    "process.env": {}
   },
   resolve: {
     alias: {
       process: "process/browser",
       stream: "stream-browserify",
       zlib: "browserify-zlib",
-      util: 'util',
-      buffer: 'buffer',
-      'bn.js': 'bn.js'
+      util: "util",
+      buffer: "buffer",
+      "bn.js": "bn.js"
     }
   },
   optimizeDeps: {
     include: ["buffer", "bn.js"],
     esbuildOptions: {
       define: {
-        global: 'globalThis'
+        global: "globalThis"
       }
     }
   }
-}) 
+});
